97426b804751706e031e202529bdb7f9d188ed3f
Slyp05 <63003495+Slyp05@users.noreply.github.com>
2025-10-05 15:33:46 +0200

    History save and load + Player prefs manager

--- Assets/Scripts/History/History.cs
+++ Assets/Scripts/History/History.cs
@@ -1,9 +1,73 @@
-﻿public static class History
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class History
 {
-    // event on new log avec catchup, gere de save et load les logs ! (dans playerprefs du coup)
+    const string PlayerPrefsKey = "HistoryLogsArray";
+
+    [Serializable]
+    struct SerializedLog
+    {
+        public LogSource source;
+        public string text;
+
+        public SerializedLog(LogSource source, string text) => (this.source, this.text) = (source, text);
+
+        public void Deconstruct(out LogSource source, out string text) => (source, text) = (this.source, this.text);
+    }
+
+    [Serializable]
+    class WrappedLogs
+    {
+        public SerializedLog[] array;
+
+        public WrappedLogs() => array = logs.ToArray();
+
+        public void Load() => logs.AddRange(array);
+    }
+
+    static readonly List<SerializedLog> logs = new();
+
+#pragma warning disable IDE1006
+    static event Action<LogSource, string> _onNewLogWithCatchUp;
+#pragma warning restore IDE1006
+
+    public static event Action<LogSource, string> OnNewLogWithCatchUp
+    {
+        add
+        {
+            foreach ((LogSource source, string text) in logs)
+                value?.Invoke(source, text);
+
+            _onNewLogWithCatchUp += value;
+        }
+        remove
+        {
+            _onNewLogWithCatchUp -= value;
+        }
+    }
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterAssembliesLoaded)]
+    static void LoadPreviousLogs()
+    {
+        if (PlayerPrefs.HasKey(PlayerPrefsKey) == false)
+            return;
+
+        string json = PlayerPrefs.GetString(PlayerPrefsKey);
+
+        JsonUtility.FromJson<WrappedLogs>(json)
+                   .Load();
+    }
 
     public static void Log(LogSource source, string text)
     {
+        logs.Add(new(source, text));
 
+        _onNewLogWithCatchUp?.Invoke(source, text);
+
+        string json = JsonUtility.ToJson(new WrappedLogs(), false);
+        PlayerPrefs.SetString(PlayerPrefsKey, json);
+        PlayerPrefs.Save();
     }
 }
