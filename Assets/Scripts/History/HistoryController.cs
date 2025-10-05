0b889cb26f923b19c2a729a4963aecf3d81c8277
Slyp05 <63003495+Slyp05@users.noreply.github.com>
2025-10-05 17:07:16 +0200

    History (Logs) done !

--- Assets/Scripts/History/HistoryController.cs
+++ Assets/Scripts/History/HistoryController.cs
@@ -1,6 +1,55 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class HistoryController : MonoBehaviour
 {
-    // gere les boutons, open, close, etc
+    [SerializeField] float tweenDuration = 0.35f;
+    [SerializeField] float backgroundOpacity = 0.7f;
+    [Space]
+    [SerializeField] Button openButton;
+    [SerializeField] Button closeButton;
+    [SerializeField] Image background;
+    [SerializeField] RectTransform panel;
+    [SerializeField] ScrollRect scroll;
+
+    float panelPositionX;
+
+    void Awake()
+    {
+        panelPositionX = -panel.rect.width;
+
+        background.color = new(background.color.r, background.color.g, background.color.b, 0f);
+        panel.anchoredPosition = new(panel.anchoredPosition.x, 0f);
+    }
+
+    void OnEnable()
+    {
+        openButton.onClick.AddListener(OpenPanel);
+        closeButton.onClick.AddListener(ClosePanel);
+    }
+
+    void OnDisable()
+    {
+        openButton.onClick.RemoveListener(OpenPanel);
+        closeButton.onClick.RemoveListener(ClosePanel);
+    }
+
+    void OpenPanel()
+    {
+        scroll.verticalNormalizedPosition = 0f;
+
+        background.DOFade(backgroundOpacity, tweenDuration);
+        panel.DOAnchorPosX(panelPositionX, tweenDuration);
+
+        background.raycastTarget = true;
+    }
+
+    void ClosePanel()
+    {
+        background.DOFade(0f, tweenDuration);
+        panel.DOAnchorPosX(0f, tweenDuration);
+
+        background.raycastTarget = false;
+    }
 }
--- Assets/Scripts/History/HistoryDisplay.cs
+++ Assets/Scripts/History/HistoryDisplay.cs
@@ -1,7 +1,44 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-// idée: log clickable ? (pour copier les code du shredder)
+//// idée: log clickable ? (pour copier les code du shredder)
 public class HistoryDisplay : MonoBehaviour
 {
-    // register a on new log de History et instancie les logs
+    [SerializeField] Sprite noneIcon;
+    [SerializeField] Sprite telephoneIcon;
+    [SerializeField] Sprite shredderIcon;
+    [Space]
+    [SerializeField] GameObject referenceObj;
+
+    Transform targetParent;
+
+    void Awake()
+    {
+        targetParent = referenceObj.transform.parent;
+
+        referenceObj.SetActive(false);
+    }
+
+    void Start() => History.OnNewLogWithCatchUp += CreateNewLog;
+
+    void OnDestroy() => History.OnNewLogWithCatchUp -= CreateNewLog;
+
+    void CreateNewLog(LogSource source, string text)
+    {
+        GameObject inst = Instantiate(referenceObj, targetParent);
+
+        inst.GetComponentInChildren<Image>().sprite = source switch
+        {
+            LogSource.None => noneIcon,
+            LogSource.Telephone => telephoneIcon,
+            LogSource.Shredder => shredderIcon,
+
+            _ => throw new System.NotImplementedException(),
+        };
+
+        inst.GetComponentInChildren<TextMeshProUGUI>().text = text;
+
+        inst.SetActive(true);
+    }
 }
