f0f15a1bd6dd306ccf9d03bcc8d3adc44b6be81e
Toupak <vitello.th@gmail.com>
2025-10-07 04:37:07 +0200

    Added more Events & Packs with Hector calls

--- Assets/Scripts/Game Loop/GameLoop.cs
+++ Assets/Scripts/Game Loop/GameLoop.cs
@@ -23,23 +23,33 @@ namespace Game_Loop
         [SerializeField] private AchievementAsset firstPage;
         [SerializeField] private AchievementAsset secondPage;
         [SerializeField] private AchievementAsset thirdPage;
+        [SerializeField] private AchievementAsset fourthPage;
 
         [Header("CardsCompletion")]
         [SerializeField] private AchievementAsset firstCard;
         [SerializeField] private AchievementAsset secondCard;
         [SerializeField] private AchievementAsset thirdCard;
         [SerializeField] private AchievementAsset fourthCard;
+        [SerializeField] private AchievementAsset fifthCard;
         [SerializeField] private AchievementAsset Completion;
 
         [Header("ShreddCompletion")]
         [SerializeField] private AchievementAsset shreddFirst;
         [SerializeField] private AchievementAsset shreddSecond;
         [SerializeField] private AchievementAsset shreddThird;
-        
+        [SerializeField] private AchievementAsset shreddFourth;
+        [SerializeField] private AchievementAsset shreddFifth;
+        [SerializeField] private AchievementAsset shreddSixth;
+        [SerializeField] private AchievementAsset shreddSeventh;
+        [SerializeField] private AchievementAsset shreddEighth;
+
         [Header("PrintCompletion")]
         [SerializeField] private AchievementAsset printFirst;
         [SerializeField] private AchievementAsset printSecond;
         [SerializeField] private AchievementAsset printThird;
+        [SerializeField] private AchievementAsset printFourth;
+        [SerializeField] private AchievementAsset printFifth;
+
 
         private bool hasBinderProc;
 
@@ -97,8 +107,18 @@ namespace Game_Loop
                 shreddFirst.Trigger();
             if (save.cardsShreddedCount >= 10)
                 shreddSecond.Trigger();
-            if (save.cardsShreddedCount >= 50)
+            if (save.cardsShreddedCount >= 25)
                 shreddThird.Trigger();
+            if (save.cardsShreddedCount >= 40)
+                shreddFourth.Trigger();
+            if (save.cardsShreddedCount >= 55)
+                shreddFifth.Trigger();
+            if (save.cardsShreddedCount >= 70)
+                shreddSixth.Trigger();
+            if (save.cardsShreddedCount >= 85)
+                shreddSeventh.Trigger();
+            if (save.cardsShreddedCount >= 100)
+                shreddEighth.Trigger();
         }
 
         private void OnPrint()
@@ -117,8 +137,12 @@ namespace Game_Loop
                 printFirst.Trigger();
             if (save.cardsPrintedCount >= 10)
                 printSecond.Trigger();
-            if (save.cardsPrintedCount >= 50)
+            if (save.cardsPrintedCount >= 25)
                 printThird.Trigger();
+            if (save.cardsPrintedCount >= 50)
+                printFourth.Trigger();
+            if (save.cardsPrintedCount >= 100)
+                printFifth.Trigger();
         }
 
         private void OnAddCard()
@@ -132,20 +156,24 @@ namespace Game_Loop
                 firstPage.Trigger();
             if (pageCount >= 3)
                 secondPage.Trigger();
-            if (pageCount >= 7)
+            if (pageCount >= 5)
                 thirdPage.Trigger();
+            if (pageCount >= 7)
+                fourthPage.Trigger();
 
             if (cardCount >= 1)
                 logHistory.Trigger();
 
-            if (cardCount >= 30)
+            if (cardCount >= 15)
                 firstCard.Trigger();
-            if (cardCount >= 50)
+            if (cardCount >= 30)
                 secondCard.Trigger();
-            if (cardCount >= 75)
+            if (cardCount >= 50)
                 thirdCard.Trigger();
-            if (cardCount >= 105)
+            if (cardCount >= 75)
                 fourthCard.Trigger();
+            if (cardCount >= 105)
+                fifthCard.Trigger();
             if (cardCount >= 106)
                 Completion.Trigger();
         }
